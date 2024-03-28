d3903513eb5730cac23d79b545b4f70203e819c7
aSwanting <gab.damico@gmail.com>
2024-03-28 14:18:54 +0100

    snack 01 (no validation)

---
+++ src/org/learning/snack01/Snack01.java
@@ -0,0 +1,21 @@
+package org.learning.snack01;
+
+import java.util.Scanner;
+
+public class Snack01 {
+    public static void main(String[] args) {
+        int userNumber;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter number between 10 and 20");
+        userNumber = Integer.parseInt(scan.nextLine());
+
+        while (userNumber < 10 || userNumber > 20) {
+            System.out.println("NO! Enter number between 10 and 20");
+            userNumber = Integer.parseInt(scan.nextLine());
+        }
+
+        System.out.println("You entered " + userNumber + ". Great Choice!");
+        scan.close();
+    }
+}
