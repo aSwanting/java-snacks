4d6597fbac2ade5f2a2350365a613d9c0887d0c8
aSwanting <gab.damico@gmail.com>
2024-03-28 15:44:24 +0100

    snack 05

---
+++ src/org/learning/snack05/Snack05.java
@@ -0,0 +1,50 @@
+package org.learning.snack05;
+
+import java.util.Scanner;
+
+public class Snack05 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String userInput = scan.nextLine();
+        scan.close();
+
+//        String userInput = "aaa222£££";
+
+        int letterCount = 0;
+        int digitCount = 0;
+        int symbolCount = 0;
+
+        for (int i = 0; i < userInput.length(); i++) {
+
+            char currentChar = userInput.charAt(i);
+            boolean isLetter = false;
+            boolean isDigit = false;
+
+            if (Character.isAlphabetic(currentChar)) isLetter = true;
+            if (Character.isDigit(currentChar)) isDigit = true;
+
+            if (isLetter) {
+                System.out.println(currentChar + " is a letter");
+                letterCount++;
+            }
+
+            if (isDigit) {
+                System.out.println(currentChar + " is a number");
+                digitCount++;
+            }
+
+            if (!isDigit && !isLetter) {
+                System.out.println(currentChar + " is a symbol");
+                symbolCount++;
+            }
+
+        }
+
+        System.out.println(userInput + " contains "
+                + letterCount + " letter(s), "
+                + digitCount + " number(s), and "
+                + symbolCount + " symbol(s)");
+    }
+}
